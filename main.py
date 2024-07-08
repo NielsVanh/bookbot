8bf352893399bf27ce1be7e3f8f825bcc41fb763
NielsVanh <niels.vanhellemont@yahoo.co.uk>
2024-07-08 15:47:46 +0200

    Update to main.py with a function to make a sorted list of character dicts from a dict of characters and a function to make a report on the frankenstein book

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
+        
         # print(file_contents)
         return file_contents
         
 def count_words():
     file_contents = main()
+    
     file_words = file_contents.split()
-    print(len(file_words))
+    
+    # print(len(file_words))
+    return len(file_words)
     
 def count_characters():
     file_contents = main().lower()
     characters = {}
+    
     for i in range(len(file_contents)):
         if file_contents[i] not in characters:
             characters[file_contents[i]] = 1
@@ -19,8 +24,33 @@ def count_characters():
             characters[file_contents[i]] += 1
     
     return characters
-     
+
+def sort_characters():
+    dict_characters = count_characters()
+    characters = []
+    
+    for character, character_count in dict_characters.items():
+        if character.isalpha():
+            characters.append({
+                "character": character,
+                "character_count": character_count
+            })
+            
+    def sort_on(dict):
+        return dict["character_count"]
+    
+    characters.sort(reverse=True, key=sort_on)
+    return characters
+    
+    
+def make_report():
+    print(f"--- Begin report of books/frankenstein.txt ---")
+    print(f"{count_words()} words found in the document")
+    print("")
+    
+    for character_dict in sort_characters():
+        print(f"The {character_dict['character']} was found {character_dict['character_count']} times")
+    
+    print("--- End report ---")
         
-# main()
-# count_words()
-print(count_characters())
+make_report()
